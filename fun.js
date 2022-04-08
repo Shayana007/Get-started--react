83e01475a48e911da0840cc879f44fbd9d2f8e43
Shayana007 <99860513+Shayana007@users.noreply.github.com>
2022-04-08 13:16:09 +0530

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Fun()
+{
+    const title ='mashed potatoes'
+    return(
+        <div>
+            <h4>{title}</h4>
+            
+        </div>
+    )
+}
+export default Fun
